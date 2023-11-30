3e3f9c1f22f22cc4c58c8e1d1e1924598824e6e3
Lina Khairutdinova <131776782+linaa-khairutdinova@users.noreply.github.com>
2023-11-30 20:59:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").bxSlider({});
+    $('.bxslider').bxSlider({
+      minSlides: 2,
+      maxSlides: 2,
+      slideWidth: 360,
+      slideMargin: 50,
+      auto: true, 
+    });
+  
+  });
